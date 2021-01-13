ef185ddeefee3a9b3ebf94835169fc9136c71269
Σ2333! <73674792+luozj1020@users.noreply.github.com>
2021-01-13 12:02:34 +0800

    Add files via upload

--- 打卡.py
+++ 打卡.py
@@ -1,32 +1,42 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import os
 import datetime
 import time
+import win32api,win32con
 
 path_init = os.path.dirname(os.path.realpath(__file__))
 
-def get():
+def get_up():	#获取用户名和密码
 	with open(path_init + '\\用户名和密码.txt', 'r', encoding = 'utf-8') as f:
 		content = f.read()
-
 	content_list = content.split('：')
 
 	username = content_list[1].split( )[0]
 	password = content_list[2]
 	return username, password
 
+def connection():	#判断网络是否连接
+	exit_code = os.system('ping www.baidu.com')
+	if exit_code:	#网络连通 exit_code == 0，否则返回非0值。
+		win32api.MessageBox(0, '请检查你的网络连接，30秒后重试', '警告', win32con.MB_OK)
+		raise Exception('connect failed.')
+
 def main():
 	driver = webdriver.Chrome(executable_path = r'C:\Users\pc\anaconda3\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe')   # 声明一个浏览器对象   指定使用chromedriver.exe路径
-	username, password = get()#获取用户名和密码
-	print(username)
-	print(password)
-	url = 'https://ehall.jlu.edu.cn/infoplus/form/BKSMRDK/start'
-	driver.get(url)  # 打开Chrome
+	username, password = get_up()#获取用户名和密码
 
-	input = driver.find_element_by_id('username')  # 通过id定位到input框
-	input.send_keys(username)   # 在输入框内输入用户名
-	input = driver.find_element_by_id('password')  # 通过id定位到input框
-	input.send_keys(password)   # 在输入框内输入密码
+	url = 'https://ehall.jlu.edu.cn/infoplus/form/BKSMRDK/start'
+	driver.get_up(url)  # 打开Chrome
+
+	wait = WebDriverWait(driver, 5)
+
+	input_username = wait.until(EC.presence_of_element_located((By.ID, 'username'))) # 显式等待通过id定位到input框
+	input_username.send_keys(username)   # 在输入框内输入用户名
+	input_password = driver.find_element_by_id('password')  # 通过id定位到input框
+	input_password.send_keys(password)   # 在输入框内输入密码
 
 	button_log_in = driver.find_element_by_id('login-submit')  # 获取登录按钮
 	button_log_in.click()  # 点击登录
@@ -58,30 +68,47 @@ def main():
 
 	driver.implicitly_wait(5)#隐式等待5秒
 
-	driver.close()               # 关闭浏览器
+	driver.close()      # 关闭浏览器
 
 sign_in_morning = False
 sign_in_night = False
 
 if __name__ == '__main__':
+	time_now = datetime.datetime.now()
+	if time_now.hour != 8 and time_now.hour != 9 and time_now.hour == 10 or time_now.hour != 21 and time_now.hour != 22:
+		print('当前不在打卡时段')
+		win32api.MessageBox(0, '当前不在打卡时段', '提示', win32con.MB_OK)
+
 	while True:
 		time_now = datetime.datetime.now()
 		if time_now.hour == 8 or time_now.hour == 9 or time_now.hour == 10:
 			sign_in_morning = True
 			if sign_in_morning == True and sign_in_night == False:
-				main()
-				sign_in_morning = False
-				sign_in_night = True
-				print('早打卡完成')
-				time.sleep(3*60*60)
+				try:
+					connection()
+					main()
+					sign_in_morning = False
+					sign_in_night = True
+					print('早打卡完成')
+					win32api.MessageBox(0, '早打卡完成，完成时间：' + str(time_now), '提示', win32con.MB_OK)
+					continue
+					time.sleep(3*60*60)
+				except:
+					time.sleep(30)
 		elif time_now.hour == 21 or time_now.hour == 22:
 			sign_in_night=True
 			if sign_in_night == True and sign_in_morning == False:
-				main()
-				sign_in_morning = True
-				sign_in_night = False
-				print('晚打卡完成')
-				time.sleep(3*60*60)
+				try:
+					connection()
+					main()
+					sign_in_morning = True
+					sign_in_night = False
+					print('晚打卡完成')
+					win32api.MessageBox(0, '晚打卡完成，完成时间：' + str(time_now), '提示', win32con.MB_OK)
+					continue
+					time.sleep(3*60*60)
+				except:
+					time.sleep(30)
 		else:
 			print(time_now)
 			time.sleep(60)
